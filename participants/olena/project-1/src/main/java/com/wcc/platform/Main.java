5638d18d3622c53a9a6b9d8043112fb9e38352e7
elena <kronverk01@gmail.com>
2026-02-26 11:24:22 +0000

    add: save members to CSV file

--- participants/olena/project-1/src/main/java/com/wcc/platform/Main.java
+++ participants/olena/project-1/src/main/java/com/wcc/platform/Main.java
@@ -4,18 +4,19 @@ package com.wcc.platform;
 import com.wcc.platform.model.Member;
 import com.wcc.platform.model.MemberRepository;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Scanner;
 
 
-public class Main {
+public class Main  {
 //CLI add User
     private static final Scanner scanner = new Scanner(System.in);
     private static final MemberRepository repository = new MemberRepository();
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 
         System.out.println("Enter name: ");
@@ -35,6 +36,7 @@ public class Main {
                 LocalDate.now()
         );
         repository.add(member);
+        repository.saveToCsv("members.csv");
         System.out.println("Member added.");
         viewMembers();
     }
--- participants/olena/project-1/src/main/java/com/wcc/platform/model/MemberRepository.java
+++ participants/olena/project-1/src/main/java/com/wcc/platform/model/MemberRepository.java
@@ -1,7 +1,10 @@
 package com.wcc.platform.model;
 
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.IOException;
+
 //repository is responsible for saving, retrieving data
 public class MemberRepository {
 
@@ -15,4 +18,21 @@ public class MemberRepository {
     public List<Member> findAll() {
         return members;
     }
+
+    public void saveToCsv(String filePath) throws IOException {
+        FileWriter writer = new FileWriter(filePath, true);
+
+        writer.write("name,email,location,joinDate%n");
+
+        for (Member member : members) {
+            writer.write(String.format("%s,%s,%s,%s%n",
+                    member.getName(),
+                    member.getEmail(),
+                    member.getLocation(),
+                    member.getJoinDate()
+            ));
+        }
+        writer.close();
+    }
 }
+
